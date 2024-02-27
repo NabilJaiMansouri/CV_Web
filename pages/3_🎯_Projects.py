992e87a0f6affee265bf6eded66742455f2e20ba
NabilJaiMansouri <nabiljaimansouri@gmail.com>
2024-02-27 15:10:56 +0100

    Modified osd tasks

--- pages/3_🎯_Projects.py
+++ pages/3_🎯_Projects.py
@@ -20,6 +20,15 @@ st.write("---")
 st.subheader("Projects")
 st.write("---")
 
+st.write(f'<div style="font-size: 15pt; font-weight: 800;"> Heart rate measurement using ECG data for epilipsy prediction</div>', unsafe_allow_html=True)
+st.write('Paddling and breaking forecasting for improved assistance of surfers.')
+if st.button('Show more', key="jednzzke"):
+    st.write(' - Deconvolution of QRSs for R-peaks detection using Attention Unet')
+    st.write(' - HR deriving and variability detection')
+    st.write(' - ')
+
+st.divider()
+
 st.write(f'<div style="font-size: 15pt; font-weight: 800;"> Stand-up assisted surfing</div>', unsafe_allow_html=True)
 st.write('Paddling and breaking forecasting for improved assistance of surfers.')
 if st.button('Show more', key="jednzzke"):
